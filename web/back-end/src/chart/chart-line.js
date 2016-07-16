e8463a7e42d3763cb4edf7607f1122550a4eae78
DingPengwei <www.dingpengwei@gmail.com>
2016-07-17 00:19:30 +0800

    chart line lib

--- web/back-end/src/chart/chart-line.js
+++ web/back-end/src/chart/chart-line.js
@@ -2,33 +2,34 @@
  * Created by dingpengwei on 7/15/16.
  */
 
-function ChartItem(name, color, data) {
+function ChartItem(name, lineColor, lineWidth, data) {
     this.name = name;
-    this.color = color;
+    this.lineColor = lineColor;
+    this.lineWidth = lineWidth;
     this.data = data;
 }
 
-function Chart() {
-    this.id = "this is the id";
-    this.year = 2016;
-    this.month = 5;
-    this.start = 1;
-    this.end = 31;
-    this.color = "#CCCCCC";
-    this.unit = 1;
-    this.line = 10;
-    this.chartItems = new Array(
-        new ChartItem("IP", "#FF00FF", new Array(2, 4, 5, 2, 3, 7, 3, 4, 5, 2, 3, 7, 2, 4, 5, 2, 10, 7, 10, 10, 5, 2, 3, 9, 2, 4, 5, 2, 3, 7, 9)),
-        new ChartItem("VP", "#FF0000", new Array(3, 4, 5, 2, 3, 7, 2, 4, 5, 2, 10, 7, 10, 10, 5, 2, 3, 9, 2, 4, 5, 2, 3, 7, 9, 2, 4, 5, 2, 3, 7)),
-        new ChartItem("VV", "#FFFF00", new Array(5, 2, 10, 7, 10, 10, 5, 2, 3, 9, 2, 4, 4, 5, 2, 3, 7, 2, 4, 9, 2, 4, 5, 2, 3, 7, 5, 2, 3, 7, 3)),
-        new ChartItem("UV", "#0000FF", new Array(3, 7, 2, 4, 5, 2, 10, 7, 10, 3, 4, 5, 2, 10, 5, 2, 3, 9, 2, 4, 5, 2, 3, 7, 9, 2, 4, 5, 2, 3, 7))
-    );
+function Chart(id,year,month,start,end,line,unit,borderColor,borderWidth,color,width,chartItems) {
+    this.id = id;
+    this.year = year; // 年份
+    this.month = month; // 月份
+    this.start = start;// 开始日期
+    this.end = end;// 结束日期
+    this.line = line;// X轴标示线条的个数
+    this.unit = unit;// 每个X轴标示线条所表示的单位
+    this.borderColor = borderColor;
+    this.borderWidth = borderWidth;
+    this.color = color;
+    this.width = width;
+    this.chartItems = chartItems;
 };
 
 //X轴上的边距
-var xPadding = 5;
+var xPaddingLeft = 5;
+var xPaddingRight = 5;
 //Y轴上的边距
-var yPadding = 5;
+var yPaddingTop = 10;
+var yPaddingBottom = 5;
 //X轴上线条的间隔
 var xOffset;
 //Y轴上线条的间隔
@@ -37,27 +38,80 @@ var yOffset;
 var textXOffset = 5;
 var textYOffset = 7;
 
-function draw() {
+function draw(chart) {
     var chartCanvas = document.getElementById("chart");
     var context = chartCanvas.getContext("2d");
-    context.imageSmoothingEnabled = false;
+    context.save();
+    context.clearRect(0, 0, chartCanvas.width, chartCanvas.height);
     context.translate(0.5, 0.5);
-    var chart = new Chart();
     drawTable(context, chartCanvas.width, chartCanvas.height, chart);
     var length = chart.chartItems.length;
     for (var index = 0; index < length; index++) {
         drawChart(context, chartCanvas.width, chartCanvas.height, chart.chartItems[index]);
     }
+    context.restore();
 }
 
+/**
+ * 仅仅显示某条线条
+ * @param itemName 显示的线条名称
+ */
+function drawItem(chart,itemName) {
+    var chartCanvas = document.getElementById("chart");
+    var context = chartCanvas.getContext("2d");
+    context.save();
+    context.clearRect(0, 0, chartCanvas.width, chartCanvas.height);
+    context.translate(0.5, 0.5);
+    drawTable(context, chartCanvas.width, chartCanvas.height, chart);
+    var length = chart.chartItems.length;
+    for (var index = 0; index < length; index++) {
+        if (itemName == chart.chartItems[index].name) {
+            drawChart(context, chartCanvas.width, chartCanvas.height, chart.chartItems[index]);
+            break;
+        }
+    }
+    context.restore();
+}
+
+/**
+ * 修改某条线的线条宽度
+ * @param itemName 线条名称
+ * @param lineWidth 线条宽度
+ */
+function drawItemByWidth(chart,itemName, lineWidth) {
+    var chartCanvas = document.getElementById("chart");
+    var context = chartCanvas.getContext("2d");
+    context.save();
+    context.clearRect(0, 0, chartCanvas.width, chartCanvas.height);
+    context.translate(0.5, 0.5);
+    drawTable(context, chartCanvas.width, chartCanvas.height, chart);
+    var length = chart.chartItems.length;
+    for (var index = 0; index < length; index++) {
+        if (itemName == chart.chartItems[index].name) {
+            chart.chartItems[index].lineWidth = lineWidth;
+        }
+        drawChart(context, chartCanvas.width, chartCanvas.height, chart.chartItems[index]);
+    }
+    context.restore();
+}
+
+/**
+ * 绘制单条数据
+ * @param context 上下文
+ * @param width 表格宽度
+ * @param height 表格高度
+ * @param chartItem 数据源
+ */
 function drawChart(context, width, height, chartItem) {
     context.save();
     context.beginPath();
-    context.strokeStyle = chartItem.color;
-    context.moveTo(xPadding, height - yPadding);
+    context.lineCap = "round";
+    context.lineWidth = chartItem.lineWidth;
+    context.strokeStyle = chartItem.lineColor;
+    context.moveTo(xPaddingLeft, height - yPaddingBottom);
     for (var i = 0; i < chartItem.data.length; i++) {
-        context.lineTo(xOffset * (i + 1) + xPadding, height - chartItem.data[i] * yOffset - yPadding);
-        context.moveTo(xOffset * (i + 1) + xPadding, height - chartItem.data[i] * yOffset - yPadding);
+        context.lineTo(xOffset * (i + 1) + xPaddingLeft, height - chartItem.data[i] * yOffset - yPaddingBottom);
+        context.moveTo(xOffset * (i + 1) + xPaddingLeft, height - chartItem.data[i] * yOffset - yPaddingBottom);
     }
     context.stroke();
     context.restore();
@@ -73,21 +127,21 @@ function drawTable(context, width, height, chart) {
     context.save();
     context.beginPath();
     //边框样式
-    context.lineWidth = chart;
-    context.strokeStyle = '#000000';
+    context.lineWidth = chart.borderWidth;
+    context.strokeStyle = chart.borderColor;
     //绘制底边框
-    context.moveTo(xPadding, height - yPadding);
-    context.lineTo(width - xPadding, height - yPadding);
+    context.moveTo(xPaddingLeft, height - yPaddingBottom);
+    context.lineTo(width - xPaddingRight, height - yPaddingBottom);
     //绘制左边框
-    context.moveTo(xPadding, height - yPadding);
-    context.lineTo(xPadding, yPadding);
+    context.moveTo(xPaddingLeft, height - yPaddingBottom);
+    context.lineTo(xPaddingLeft, yPaddingTop);
     //绘制坐标原点文字
     context.fillText("0", textXOffset, height - textYOffset);
     context.stroke();
-
+    context.save();
     //数据标示线样式
     context.beginPath();
-    context.lineWidth = chart;
+    context.lineWidth = chart.width;
     context.strokeStyle = chart.color;
 
     // 绘制纵向的数据标示线条
@@ -95,24 +149,37 @@ function drawTable(context, width, height, chart) {
     // 假设每个月都是31天
     // 实际绘制按照参数进行
     var xCounter = 31 + 1; //31条线分割出32个空间
-    xOffset = (width - 2 * xPadding) / xCounter;
+    xOffset = (width - xPaddingLeft - xPaddingRight) / xCounter;
     for (var index = chart.start; index < chart.end + 1; index++) {
-        var startX = xPadding + index * xOffset;
-        context.moveTo(startX, yPadding);
-        context.lineTo(startX, height - yPadding);
+        var startX = xPaddingLeft + index * xOffset;
+        context.moveTo(startX, yPaddingTop);
+        context.lineTo(startX, height - yPaddingBottom);
         context.fillText(append(index, 2), startX - textXOffset, height - textYOffset);
     }
 
     // 绘制横向的数据标示线条
     var yCounter = chart.line;
-    yOffset = (height - 2 * yPadding) / (yCounter + 1);
+    yOffset = (height - yPaddingTop - yPaddingBottom) / (yCounter + 1);
     for (var index = 1; index < yCounter + 1; index++) {
-        var startY = height - yPadding - yOffset * index;
-        context.moveTo(xPadding, startY);
-        context.lineTo(width - xPadding, startY);
-        context.fillText(append(index, 2), xPadding, startY);
+        var startY = height - yPaddingBottom - yOffset * index;
+        context.moveTo(xPaddingLeft, startY);
+        context.lineTo(width - xPaddingLeft, startY);
+        context.fillText(append(index, 2), xPaddingLeft, startY);
     }
+    context.stroke();
+    context.restore();
 
+    context.beginPath();
+    //绘制底部边框的单位
+    context.fillText("号", width - xPaddingRight - xOffset + 2 * textXOffset, height - textYOffset);
+    context.stroke();
+    context.restore();
+
+    context.save();
+    context.beginPath();
+    context.strokeStyle = '#FF00FF';
+    //绘制左边边框的单位
+    context.fillText(chart.unit, textXOffset, yPaddingTop);
     context.stroke();
     context.restore();
 }
@@ -126,54 +193,3 @@ function append(num, n) {
     return num;
 }
 
-window.onload = draw();
-/*
- //该数据结构描述了某年某月的流量数据
- {"id":"this is the id","year":"2016","month":"05","start":"01","end":"31","data":[{"name":"IP","color":"#FF00FF","data":["20","20","20"]},{"name":"PV","color":"#FF00FF","data":["20","20","20"]},{"name":"VV","color":"#FF00FF","data":["20","20","20"]},{"name":"UV","color":"#FF00FF","data":["20","20","20"]}]}
-
- {
- "id": "this is the id",
- "year": "2016",
- "month": "05",
- "start": "01",
- "end": "31",
- "data": [
- {
- "name": "IP",
- "color": "#FF00FF",
- "data": [
- "20",
- "20",
- "20"
- ]
- },
- {
- "name": "PV",
- "color": "#FF00FF",
- "data": [
- "20",
- "20",
- "20"
- ]
- },
- {
- "name": "VV",
- "color": "#FF00FF",
- "data": [
- "20",
- "20",
- "20"
- ]
- },
- {
- "name": "UV",
- "color": "#FF00FF",
- "data": [
- "20",
- "20",
- "20"
- ]
- }
- ]
- }
- */
