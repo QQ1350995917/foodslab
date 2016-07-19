2189635f320601cbc48c5e56e3bf080a1a53c6f6
DingPengwei <www.dingpengwei@gmail.com>
2016-07-19 16:04:26 +0800

    progress & toast

--- web/back-end/src/progress/progress.js
+++ web/back-end/src/progress/progress.js
@@ -1,41 +1,58 @@
 /**
  * Created by dingpengwei on 7/18/16.
  */
-function CircleProgress(rotateAngle, id) {
+/**
+ * 定义CircleProgress对象属性
+ * @param id canvas的ID
+ * @constructor
+ */
+function CircleProgress(id) {
     this.progress = document.getElementById(id);
     this.context = this.progress.getContext("2d");
-    this.radius = Math.min(this.progress.width, this.progress.height) / 3;
     this.centerX = this.progress.width / 2;
     this.centerY = this.progress.height / 2;
-    this.context.translate(this.centerX,this.centerY);
+    this.radius = Math.min(this.progress.width, this.progress.height) / 3;
+    this.context.translate(this.centerX, this.centerY);
 }
 
+/**
+ * 绘制
+ */
 CircleProgress.prototype.drawCircleProgress = function () {
-    this.context.clearRect(0-this.centerX,0-this.centerY,2*this.centerX,2*this.centerY);
-    var initRadius = 15;
+    this.context.clearRect(0 - this.centerX, 0 - this.centerY, 2 * this.centerX, 2 * this.centerY);
+    var preRadius = 15;
     var preAngle = 0;
-    var currentRadius = initRadius;
+    var currentRadius = preRadius;
     for (var index = 0; index < 15; index++) {
-        var currentAngle = Math.acos((2 * Math.pow(this.radius, 2) - Math.pow((initRadius + currentRadius), 2)) / (2 * Math.pow(this.radius, 2))) * (180 / Math.PI);
-        var location = getArcLocation(this.radius, - (currentAngle + preAngle) * Math.PI / 180);
+        var currentAngle = Math.acos(((2 * Math.pow(this.radius, 2) - Math.pow((preRadius + currentRadius), 2)) - 5) / (2 * Math.pow(this.radius, 2))) * (180 / Math.PI);
+        var location = getArcLocation(this.radius, -(currentAngle + preAngle) * Math.PI / 180);
+        this.context.fillStyle = '#FFFFFF';
         this.context.beginPath();
-        this.context.fillStyle = "#FFFFFF"
         this.context.arc(location[0], location[1], currentRadius, 0 * Math.PI, 2 * Math.PI, true);
         this.context.closePath();
         this.context.fill();
+        preRadius = currentRadius;
         currentRadius--;
         preAngle += currentAngle;
     }
 }
 
-
+/**
+ * 根据半径和弧度计算绘制坐标
+ * @param radius 大圆半径
+ * @param arc 偏差弧度
+ * @returns {Array} 小圆圆心坐标
+ */
 function getArcLocation(radius, arc) {
     var x = radius * Math.cos(arc);
     var y = radius * Math.sin(arc);
     return new Array(x, y);
 }
 
-
+/**
+ * 给CircleProgress添加旋转函数
+ * @param interval
+ */
 CircleProgress.prototype.rotate = function (interval) {
     this.drawCircleProgress();
     var progress = this;
@@ -45,7 +62,11 @@ CircleProgress.prototype.rotate = function (interval) {
     }, interval);
 }
 
-//http://www.cnblogs.com/varlxj/p/taiji.html
+/**
+ * 显示进度
+ * 动态创建显示对象
+ * 并且屏蔽相关的键盘鼠标事件
+ */
 function showLoadingView() {
     var mask = document.createElement('div');
     mask.id = "mask";
@@ -59,43 +80,66 @@ function showLoadingView() {
     mask.style.opacity = "0.6";
     document.body.appendChild(mask);
 
+    var canvas = document.createElement('canvas');
+    canvas.id = "canvas";
+    canvas.style.backgroundColor = "#00FFFFFF";
+    canvas.style.width = document.body.clientWidth / 5 + "px";
+    canvas.style.height = document.body.clientHeight / 5 + "px";
+    canvas.style.left = 2 * document.body.clientWidth / 5 + "px";
+    canvas.style.top = document.body.clientHeight / 5 + "px";
+    canvas.style.zIndex = "10";
+    canvas.style.position = "absolute";
+    canvas.style.opacity = "0.9";
+    document.body.appendChild(canvas);
+
+
     var button = document.createElement('button');
     button.id = "button";
+    button.style.styleFloat = "left";
     button.innerHTML = "close";
-    button.style.width = "200px";
-    button.style.height = "200px";
+    button.style.width = document.body.clientWidth / 5 + "px";
+    button.style.height = "100px";
+    button.style.left = 2 * document.body.clientWidth / 5 + "px";
+    button.style.top = 2 * document.body.clientHeight / 5 + "px";
     button.style.zIndex = "10";
-    button.style.position = "relative";
+    button.style.position = "absolute";
     button.style.backgroundColor = "red";
     button.style.opacity = "0.9";
     button.style.cursor = "pointer";
     button.addEventListener("click", dismissLoadingView);
     document.body.appendChild(button);
 
-    var canvas = document.createElement('canvas');
-    canvas.id = "canvas";
-    canvas.style.backgroundColor = "00FFFFFF";
-    canvas.style.width = "400";
-    canvas.style.height = "400";
-    canvas.style.left = "400px";
-    canvas.style.top = "100px";
-    canvas.style.position = "absolute";
-    canvas.style.zIndex = "10";
-    canvas.style.opacity = "0.6";
-    document.body.appendChild(canvas);
-
-
     document.documentElement.style.overflow = 'hidden';
 
     clearInterval(CircleProgress.prototype.task);
-    var progress = new CircleProgress(200, 'canvas');
+    var progress = new CircleProgress('canvas');
     progress.rotate(30);
 
+    // document.onkeydown = onkeydown;
+
+    document.oncontextmenu = new Function("event.returnValue=false;");
+    document.onselectstart = new Function("event.returnValue=false;");
+    window.onhelp = new Function("event.returnValue=false;"); //屏蔽F1帮助
+
+
+    document.onkeydown = function () {
+        if (window.event && window.event.keyCode == 13) {
+            window.event.returnValue = false;
+        }
+    }
 }
 
+/**
+ * 移除动态创建的view对象
+ * 并回复键盘鼠标事件
+ */
 function dismissLoadingView() {
+    document.onkeydown = null;
     document.body.removeChild(document.getElementById('button'));
     document.body.removeChild(document.getElementById('mask'));
+    document.body.removeChild(document.getElementById('canvas'));
     document.documentElement.style.overflow = 'scroll';
     clearInterval(CircleProgress.prototype.task);
+    document.oncontextmenu = new Function("event.returnValue=true;");
+    document.onselectstart = new Function("event.returnValue=true;");
 }
